af191af1aaee9f64a97b5494ff60cfa2adea6ceb
Andrew-Garanin <andrey-garanin-00@mail.ru>
2022-03-26 12:24:42 +0300

    Added timer

--- app/src/main/java/com/example/truthordaregame/gamecontent/GameContentFragment.kt
+++ app/src/main/java/com/example/truthordaregame/gamecontent/GameContentFragment.kt
@@ -1,6 +1,7 @@
 package com.example.truthordaregame.gamecontent
 
 import android.os.Bundle
+import android.text.format.DateUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.truthordaregame.R
 import com.example.truthordaregame.databinding.FragmentGameContentBinding
@@ -40,9 +42,28 @@ class GameContentFragment : Fragment() {
             binding.textContent.text = newContent
         })
 
+        viewModel.eventGameFinish.observe(viewLifecycleOwner, {   finished ->
+            if (finished)
+                findNavController().navigateUp()
+        })
+
+        viewModel.secondsUntilEnd.observe(viewLifecycleOwner, { secondsUntilEnd ->
+            binding.textTimer.text = DateUtils.formatElapsedTime(secondsUntilEnd)
+        })
+
         binding.buttongoNext.setOnClickListener{
             it.findNavController().navigateUp()
         }
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.resumeTimer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        viewModel.pauseTimer()
     }
 }
--- app/src/main/java/com/example/truthordaregame/gamecontent/GameContentViewModel.kt
+++ app/src/main/java/com/example/truthordaregame/gamecontent/GameContentViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.truthordaregame.gamecontent
 
+import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -13,8 +14,53 @@ class GameContentViewModel(content: String, contentType: Int) : ViewModel() {
     val contentType: LiveData<Int>
         get() = _contentType
 
+    private var _eventGameFinish = MutableLiveData(false)
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
+
+    private var _secondsUntilEnd = MutableLiveData<Long>()
+    val secondsUntilEnd : LiveData<Long>
+        get() = _secondsUntilEnd
+
+    private var _isTimerPaused = MutableLiveData(false)
+    val isTimerPaused : LiveData<Boolean>
+        get() = _isTimerPaused
+
+    //private var timer: CountDownTimer
+
+    companion object{
+        const val ONE_SECOND = 1000L
+        const val GAME_TIME = 10 * ONE_SECOND
+    }
+
     init {
         _content.value = content
         _contentType.value = contentType
+        _secondsUntilEnd.value = GAME_TIME / ONE_SECOND
+
+       // timer(_secondsUntilEnd.value!! * ONE_SECOND).start()
+    }
+
+    private fun timer(millisInFuture:Long):CountDownTimer{
+        return object: CountDownTimer(millisInFuture, ONE_SECOND){
+            override fun onTick(millisUntilFinished: Long){
+                _secondsUntilEnd.value = millisUntilFinished / ONE_SECOND
+
+                if (_isTimerPaused.value == true){
+                    cancel()
+                }
+            }
+
+            override fun onFinish() {
+                _eventGameFinish.value = true
+            }
+        }
+    }
+    fun pauseTimer(){
+        _isTimerPaused.value = true
+    }
+    fun resumeTimer(){
+        _isTimerPaused.value = false
+        timer(_secondsUntilEnd.value!! * ONE_SECOND).start()
     }
 }
